cd419b8e2012a4a578f9c6b4db4d291c4c6b9550
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-10-21 06:45:32 +0330

    Tested More Examples of Tuples

---
+++ TupleExample2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TupleExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = TupleSample.GetNewTuple();
+            Console.WriteLine($"First Name : {person.Item1} , LastName : {person.Item2} , Age : {person.Item3}");
+
+            var Info = TupleSample.GetPerson();
+            Console.WriteLine($"{Info.firstName} - {Info.lastName} - {Info.age}");
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class TupleSample
+    {
+        public static (string, string, int) GetNewTuple()
+        {
+            string firstName = "Mostafa";
+            string lastName = "Kazemi";
+            int age = 24;
+
+            return (firstName, lastName, age);
+        }
+
+        public static (string firstName, string lastName, int age) GetPerson()
+        {
+            string firstName = "Mostafa";
+            string lastName = "Kazemi";
+            int age = 24;
+
+            return (firstName, lastName, age);
+        }
+    }
+}
