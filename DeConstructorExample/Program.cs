42893a863da6fa6f800c57fe23908e263907c80c
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-10-22 23:00:52 +0330

    Used DeConstruct and Tuples in this Example

--- DeConstructorExample/Program.cs
+++ DeConstructorExample/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DeConstructorExample
+namespace DeConstructExample
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Person p = new Person("Mostafa", "Kazemi");
+            var (firstName, lastName) = p;
+            Console.WriteLine($"First Name : {firstName} Last Name : {lastName}");
+
+            //Console.WriteLine($"First Name : {p.FirstName} Last Name : {p.LastName}");
+        }
+    }
+
+    class Person
+    {
+        private string FirstName { get; }
+        private string LastName { get; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public void Deconstruct(out string firstName, out string lastName)
+        {
+            firstName = FirstName;
+            lastName = LastName;
         }
     }
 }
