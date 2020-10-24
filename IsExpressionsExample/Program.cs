33f455cd3fcd8546fc2b2de494204f51a9d9b2d9
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-10-24 19:29:08 +0330

    Tested IsExpression to get parent class from a child class

--- IsExpressionsExample/Program.cs
+++ IsExpressionsExample/Program.cs
@@ -10,6 +10,38 @@ namespace IsExpressionsExample
     {
         static void Main(string[] args)
         {
+
+            PersonCar pc = new PersonCar("Mostafa", "Kazemi", 30, "Tesla", 3);
+
+            if (pc is Person p)
+            {
+                Console.WriteLine($"Name : {p.Name} , Family : {p.Family} , Age : {p.Age}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public int Age { get; set; }
+    }
+
+    class PersonCar : Person
+    {
+        public string CarName { get; set; }
+        public int CarModel { get; set; }
+
+
+        public PersonCar(string name, string family, int age, string carName, int carModel)
+        {
+            Name = name;
+            Family = family;
+            Age = age;
+            CarName = carName;
+            CarModel = carModel;
         }
     }
 }
