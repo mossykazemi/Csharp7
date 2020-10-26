6f76335563381bda02fd57f026c3819f5d29844e
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-10-26 22:49:39 +0330

    Used ValueTask

---
+++ Async2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Async2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Please Wait ...";
+            string name = await SayHello();
+            label1.Text = "Hey " + name;
+        }
+
+        async Task<string> GetName()
+        {
+            await Task.Delay(5000);
+            return "Mostafa Kazemi";
+        }
+
+        ValueTask<string> SayHello()
+        {
+            return new ValueTask<string>(GetName());
+        }
+    }
+}
