e99a34581ae2ae55df2328efa9c49f520afd303f
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-10-27 20:35:03 +0330

    Before Use Arrow Function

--- MoreExamples/Program.cs
+++ MoreExamples/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,39 @@ namespace MoreExamples
 {
     class Program
     {
+        //private static string _name = "Mostafa Kazemi";
+
+        //public static string Name
+        //{
+        //    get { return _name;}
+        //    set { _name = value; }
+        //}
+
+        public static string Name => "Mostafa Kazemi";
+
         static void Main(string[] args)
         {
+            // Console.WriteLine(Name);
+
+            Person p = new Person("Mostafa");
+            Console.WriteLine(p);
+        }
+    }
+
+    class Person
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public Person(string name)
+        {
+            _name = name;
         }
     }
 }
+
