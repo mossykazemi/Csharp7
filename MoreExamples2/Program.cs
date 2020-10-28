7d757ede3577088e6b3c7040b3b551b24e74e865
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-10-28 19:12:03 +0330

    checked Null another way

--- MoreExamples2/Program.cs
+++ MoreExamples2/Program.cs
@@ -17,14 +17,23 @@ namespace MoreExamples2
 
         static void Main(string[] args)
         {
-            Name = null;
-
+            //Name = null;
 
             //if (Name == null)
             //    throw new ArgumentNullException(nameof(Name),"Name Must Not be null");
 
+            //Console.WriteLine(Name);
 
-            Console.WriteLine(Name);
+
+            Person p = new Person();
+            p.Name = "Mossy";
+            Console.WriteLine($"Name : {p?.Name}"); // check to see if it's null
         }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+
     }
 }
