c52899d6c699dee12284e10789f1dce84441e92d
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-10-28 19:05:39 +0330

    Before Any Changes

---
+++ MoreExamples2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreExamples2
+{
+    class Program
+    {
+        private static string _name = "Mostafa";
+        public static string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        static void Main(string[] args)
+        {
+            Name = null;
+
+
+            if (Name == null)
+                throw new ArgumentNullException(nameof(Name),"Name Must Not be null");
+
+
+            Console.WriteLine(Name);
+        }
+    }
+}
